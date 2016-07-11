0952bc1367ca1afdbf07c1871f3defd876ec57ef
Ron <rao261488647@126.com>
2016-07-11 23:43:23 +0800

    家里的版本

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -1,4 +1,4 @@
-"""MyBlog URL Configuration
+"""myBlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from blog.views import hello
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^hello/', hello),
 ]
