81e9b964e625f17af10ed73403fbb2b9d38eb897
Ron <rao261488647@126.com>
2016-07-12 01:53:41 +0800

    增加user类

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+#coding=utf-8
 from django.db import models
 
 # Create your models here.
+
+'''
+用户类
+Ron
+2016-07-11
+'''
+
+class Users(models.Model):
+    userName = models.CharField(max_length = 64)
+    passWord = models.CharField(max_length = 64)
+    validStatus = models.CharField(max_length = 1)
